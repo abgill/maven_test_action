369a2df1d6e2ad5cdc32c9216f978435668805ad
Andrew Gill <Andrew.Gill@familysearch.org>
2022-04-26 16:25:33 -0600

    Ls dir

--- dist/index.js
+++ dist/index.js
@@ -1975,6 +1975,10 @@ const location = core.getInput('location');
 
 console.log(`Copying file at "${location}" to ~/.m2/settings.xml`);
 
+fs.readdirSync('./').forEach(file => {
+    console.log(file);
+});
+
 fs.copyFileSync(location, "~/.m2/settings.xml");
 })();
 
--- index.js
+++ index.js
@@ -6,4 +6,8 @@ const location = core.getInput('location');
 
 console.log(`Copying file at "${location}" to ~/.m2/settings.xml`);
 
+fs.readdirSync('./').forEach(file => {
+    console.log(file);
+});
+
 fs.copyFileSync(location, "~/.m2/settings.xml");
