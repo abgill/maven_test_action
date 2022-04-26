aec059885f0ae9e3da7e2ba99df0b93c2308ca5c
Andrew Gill <Andrew.Gill@familysearch.org>
2022-04-26 16:42:01 -0600

    Mkdir if it does not exist

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ fs.readdirSync('./').forEach(file => {
 
 const m2Dir = path.join(os.homedir(), '.m2', 'settings.xml');
 
+if (!fs.existsSync(path.dirname(m2Dir))) {
+    core.info("creating directory for settings.xml: " + m2Dir);
+    fs.mkdirSync(path.dirname(m2Dir));
+}
+
 fs.copyFileSync(location, m2Dir);
 
 console.log(`Copying file at "${location}" to ${m2Dir}`);
