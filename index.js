f2bc8fe59a33795c462ddd80513ebaca95d5ec85
Andrew Gill <Andrew.Gill@familysearch.org>
2022-04-26 16:17:35 -0600

    Copy settings xml

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require('fs');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+
+const location = core.getInput('location');
+
+console.log(`Copying file at "${location}" to ~/.m2/settings.xml`);
+
+fs.copyFileSync(location, "~/.m2/settings.xml");
